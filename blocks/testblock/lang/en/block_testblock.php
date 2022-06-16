5225257a3b1bbbb117e24506e0aaa767f3472a09
Ajay <ajay.chukka@peopletech.com>
2022-06-16 16:55:55 +0530

    Setting with lang

--- blocks/testblock/lang/en/block_testblock.php
+++ blocks/testblock/lang/en/block_testblock.php
@@ -3,3 +3,5 @@ $string['testblock:addinstance'] = 'Add a new test block';
 $string['testblock:myaddinstance'] = 'Add a new test block to Dashboard';
 $string['testblock'] = 'Test Block';
 $string['pluginname'] = 'test block';
+$string['showcourses'] = 'Show Courses';
+$string['coursedesc'] = 'Select this checkbox to display the courses in New test block';
--- blocks/testblock/settings.php
+++ blocks/testblock/settings.php
@@ -1,4 +1,5 @@
 <?php
 if($ADMIN->fulltree){
-    $settings->add(new admin_setting_configcheckbox('block_testblock/showcourses', 'Show Courses', 'Show Courses instead of users',0));
+    $settings->add(new admin_setting_configcheckbox('block_testblock/showcourses', 
+        get_string('showcourses','block_testblock'), get_string('coursedesc','block_testblock'),0));
 }
