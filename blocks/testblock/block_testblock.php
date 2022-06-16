4b6e5bef79ab5a0d48e33ef602eddc2a080c91d8
Ajay <ajay.chukka@peopletech.com>
2022-06-16 16:19:52 +0530

    Fetch User from db to test block

--- blocks/testblock/block_testblock.php
+++ blocks/testblock/block_testblock.php
@@ -6,11 +6,20 @@ class block_testblock extends block_base {
     }
 
     public function get_content() {
+        global $DB;
+
         if ($this->content !== NULL) {
             return $this->content;
         }
+
+        $users = $DB->get_records('user');
+        $userstring = '';
+        foreach($users as $user){
+            $userstring .=$user->firstname." ".$user->lastname."<br/>";
+        }
+
         $this->content = new stdClass;
-        $this->content->text = 'This is text'; 
+        $this->content->text = $userstring; 
         $this->content->footer = 'This is footer';
         return $this->content;
     }
